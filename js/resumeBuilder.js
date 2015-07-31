d4551868f00690c83e89fffd15240f980d63e603
YaroslavShemshur <yarik33333@gmail.com>
2015-07-31 20:22:09 +0300

    Full objects : bio, work, education!

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,94 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+
+var bio = {
+	"name" : "Yaroslav",
+	"role" : "Web developer",
+	"contactInfo" : {
+		"mobile" : "359564864",
+		"email" : "fskdok@sdsds",
+		"github" : "YaroslavShemshur",
+		"twitter" : "Me",
+		"location" : "City!"
+	},
+	"picture" : "images/me.jpg",
+	"welcomeMessage" : "Hi",
+	"skills" : ["JS ","Full stack developer "]
+}
+var work = {
+	"job" : [
+		{ 
+			"location" : "Cherkasy",
+			"employer" : "no",
+			"yearsWorked" : "2",
+			"description" : "Cherkasy old"
+		},
+		{
+			"location" : "Kyiv",
+			"employer" : "Second",
+			"yearsWorked" : "1",
+			"description" : "Nice"
+		}
+	]
+}
+
+var education = {
+	"school" : [
+		{
+			"name" : "name of the school",
+			"location" : "Cherkasy",
+			"deegre" : "school education",
+			"majors" : ["1","2"],
+			"dates" : "2005-2010"
+		},
+		{
+			"name" : "name of the school",
+			"location" : "Kyiv",
+			"deegre" : "school education",
+			"majors" : ["1","2"],
+			"dates" : "2005-2010"
+		}
+	],
+	"onlineCourses" : [
+		{
+			"title" : "title(courses)",
+			"school" : "Udacity",
+			"dates" : "Time(courses)",
+			"url" : "url(courses)"
+		},
+		{
+			"title" : "title(courses)",
+			"school" : "Coursera",
+			"dates" : "Time(courses)",
+			"url" : "url(courses)"
+		}
+	]
+}
+var projects = {
+	"projects" : [
+		{
+			"tittle" : "RPG",
+			"dates" : "2008-2010",
+			"description" : "Cool!",
+			"image" : "url"
+		},
+		{
+			"tittle" : "CLicker",
+			"dates" : "2010-2012",
+			"description" : "Cool!",
+			"image" : "url"
+		}
+	]
+}
+
+/*$("#education").prepend(education["yearsInTheSchool"]);
+$("#education").prepend(education["schoolCity"]);
+$("#workExperience").prepend(work.jobPosition);
+$("#workExperience").prepend(work.employer);
+$("#workExperience").prepend(work.yearsWorked);
+$("#workExperience").prepend(work.cityOfTheBusiness);
+$("#header").prepend(bio.role);
+$("#header").prepend(bio.name);
+$("#header").prepend(bio.skills);
+$("#main").prepend(bio.welcomeMessage);
+$("#main").prepend(bio.picture);
+$("#main").prepend(bio.contactInfo);
+*/
